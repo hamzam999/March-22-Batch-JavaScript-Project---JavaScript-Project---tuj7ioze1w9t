3d9205b1b3569deca812430134ad926b69347431
hamzam999 <hamza.m999@gmail.com>
2022-09-27 02:17:06 +0530

    tip calc

--- App.js
+++ App.js
@@ -1,1 +1,35 @@
+function custom_input() {
+  if (document.getElementById('tip_radio').checked == true) {
+    document.getElementById('custom_percent').setAttribute('required', '')
+    // console.log('custom clicked')
+  }
+}
 
+function calculateTip() {
+  var bill = document.querySelector('#billamt').value
+  var tipPercentage = 0
+  if (document.querySelector('input[name = "tip_percent"]:checked') != null) {
+    tipPercentage = document.querySelector(
+      'input[name="tip_percent"]:checked'
+    ).value
+  }
+  //   if (document.getElementById('tip_radio').checked == true) {
+  //     document.getElementById('custom_percent').setAttribute('required', '')
+  //     console.log('custom clicked')
+  //   }
+  if (tipPercentage === 'custom') {
+    tipPercentage = document.getElementById('custom_percent').value
+    tipPercentage = tipPercentage / 100
+  }
+  //   console.log(tipPercentage)
+  var persons = document.querySelector('#peopleamt').value
+
+  if (persons === '1') {
+    document.querySelector('#each').style.display = 'none'
+  } else {
+    document.querySelector('#each').style.display = 'block'
+  }
+  var tip = (bill * tipPercentage) / persons
+  tip = tip.toFixed(2)
+  document.getElementById('total').innerHTML = tip
+}
